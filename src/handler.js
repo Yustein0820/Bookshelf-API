62ac84b587b9176b4b5784656c65c3b943879ba6
Yustein0820 <yusteind@gmail.com>
2022-08-10 19:58:57 +0700

    last commit

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,4 @@
 const { nanoid } = require('nanoid');
-// files
 const books = require('./books');
 
 const addBookHandler = (request, h) => {
@@ -15,23 +14,21 @@ const {
 } = request.payload;
 
 if (!name) {
-    // Client tidak melampirkan properti name pada request body
     const response = h
     .response({
         status: 'fail',
-        message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        message: 'Gagal menambahkan data buku. Mohon isi dahulu nama buku',
     })
     .code(400);
     return response;
 }
 
 if (readPage > pageCount) {
-    // Client melampirkan nilai properti readPage yang lebih besar dari nilai properti pageCount
     const response = h
     .response({
         status: 'fail',
         message:
-        'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        'Gagal menambahkan data buku. readPage tidak dibolehkan lebih besar dari pada pageCount',
     })
     .code(400);
     return response;
@@ -57,16 +54,15 @@ const newBook = {
     updatedAt,
 };
 
-  books.push(newBook); // push to books array
+  books.push(newBook);
 
-  const isSuccess = books.filter((note) => note.id === id).length > 0; // cek if newBook pushed
+  const isSuccess = books.filter((note) => note.id === id).length > 0;
 
 if (isSuccess) {
-    // Bila buku berhasil dimasukkan
     const response = h
     .response({
         status: 'success',
-        message: 'Buku berhasil ditambahkan',
+        message: 'Buku sudah berhasil ditambahkan',
         data: {
         bookId: id,
         },
@@ -75,11 +71,10 @@ if (isSuccess) {
     return response;
 }
 
-  // Server gagal memasukkan buku karena alasan umum (generic error).
 const response = h
     .response({
     status: 'fail',
-    message: 'Buku gagal ditambahkan',
+    message: 'Buku gagal untuk ditambahkan',
     })
     .code(500);
 return response;
@@ -89,7 +84,6 @@ const getAllBooksHandler = (request, h) => {
 const { name, reading, finished } = request.query;
 
 if (!name && !reading && !finished) {
-    // kalau tidak ada query
     const response = h
     .response({
         status: 'success',
@@ -108,7 +102,6 @@ if (!name && !reading && !finished) {
 
 if (name) {
     const filteredBooksName = books.filter((book) => {
-      // kalau ada query name
     const nameRegex = new RegExp(name, 'gi');
     return nameRegex.test(book.name);
     });
@@ -130,7 +123,6 @@ if (name) {
 }
 
 if (reading) {
-    // kalau ada query reading
     const filteredBooksReading = books.filter(
     (book) => Number(book.reading) === Number(reading),
     );
@@ -151,7 +143,6 @@ if (reading) {
     return response;
 }
 
-  // kalau ada query finished
 const filteredBooksFinished = books.filter(
     (book) => Number(book.finished) === Number(finished),
 );
@@ -178,7 +169,6 @@ const { bookId } = request.params;
   const book = books.filter((n) => n.id === bookId)[0]; // find book by id
 
 if (book) {
-    // Bila buku dengan id yang dilampirkan ditemukan
     const response = h
     .response({
         status: 'success',
@@ -190,11 +180,10 @@ if (book) {
     return response;
 }
 
-  // Bila buku dengan id yang dilampirkan oleh client tidak ditemukan
 const response = h
     .response({
     status: 'fail',
-    message: 'Buku tidak ditemukan',
+    message: 'Buku tidak ada ditemukan',
     })
     .code(404);
 return response;
@@ -215,23 +204,21 @@ const {
 } = request.payload;
 
 if (!name) {
-    // Client tidak melampirkan properti name pada request body
     const response = h
     .response({
         status: 'fail',
-        message: 'Gagal memperbarui buku. Mohon isi nama buku',
+        message: 'Gagal memperbarui data buku. Mohon isi dahulu nama buku',
     })
     .code(400);
     return response;
 }
 
 if (readPage > pageCount) {
-    // Client melampirkan nilai properti readPage yang lebih besar dari nilai properti pageCount
     const response = h
     .response({
         status: 'fail',
         message:
-        'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+        'Gagal memperbarui data buku. readPage tidak boleh lebih besar pada dari pageCount',
     })
     .code(400);
     return response;
@@ -240,7 +227,7 @@ if (readPage > pageCount) {
 const finished = pageCount === readPage;
 const updatedAt = new Date().toISOString();
 
-  const index = books.findIndex((note) => note.id === bookId); // find book by id
+  const index = books.findIndex((note) => note.id === bookId);
 
 if (index !== -1) {
     books[index] = {
@@ -257,21 +244,19 @@ if (index !== -1) {
     updatedAt,
     };
 
-    // Bila buku berhasil diperbarui
     const response = h
     .response({
         status: 'success',
-        message: 'Buku berhasil diperbarui',
+        message: 'Buku sudah berhasil diperbarui',
     })
     .code(200);
     return response;
 }
 
-  // id yang dilampirkan oleh client tidak ditemukkan oleh server
 const response = h
     .response({
     status: 'fail',
-    message: 'Gagal memperbarui buku. Id tidak ditemukan',
+    message: 'Gagal memperbarui data buku. Id buku tidak ditemukan',
     })
     .code(404);
 return response;
@@ -280,26 +265,24 @@ return response;
 const deleteBookByIdHandler = (request, h) => {
 const { bookId } = request.params;
 
-  const index = books.findIndex((note) => note.id === bookId); // find book by id
+  const index = books.findIndex((note) => note.id === bookId);
 
 if (index !== -1) {
     books.splice(index, 1);
 
-    // Bila id dimiliki oleh salah satu buku
     const response = h
     .response({
         status: 'success',
-        message: 'Buku berhasil dihapus',
+        message: 'Buku sudah berhasil dihapus',
     })
     .code(200);
     return response;
 }
 
-  // Bila id yang dilampirkan tidak dimiliki oleh buku manapun
 const response = h
     .response({
     status: 'fail',
-    message: 'Buku gagal dihapus. Id tidak ditemukan',
+    message: 'Buku gagal untuk dihapus. Id buku tidak ditemukan',
     })
     .code(404);
 return response;
